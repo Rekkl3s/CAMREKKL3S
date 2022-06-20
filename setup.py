7e629e0e56f14820413b0611192b009d963327ba
Rekkl3s <armandcalajan_666@hotmail.com>
2022-06-20 17:19:05 -0500

    Modificado y Creado por Rekkl3s

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(name='rekkles',
       version='1.0.0',
       description='rekkles is a tool that exploits several vulnerabilities in popular DVR cameras to obtain network camera credentials.',
-      url='https://github.com/EntySec/rekkles',
+      url='https://github.com/Rekkl3s/rekkles',
       author='EntySec',
       author_email='alastorkls82@gmail.com',
       license='MIT',
